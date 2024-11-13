10e91b0667817be0db91b44bfcdf81006ab407cb
mizamarzes <mizamarzes@gmail.com>
2024-11-13 13:11:25 -0500

    refactor: pr-13

--- app/page.js
+++ app/page.js
@@ -1,17 +1,15 @@
 "use client" 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import React from 'react'; 
- import Navbar from './components/navbar';
- import HomePage from './pages/HomePg';
+import Navbar from './components/navbar';
+import HomePage from './Pages/HomePg';
 
 export default function Home() {
- 
-
+   
   return (
    <>
     <Navbar />
-    
-<HomePage />
+    <HomePage />
    </>
   );
 }
